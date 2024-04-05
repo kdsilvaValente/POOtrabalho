e8adef6b0c7624beb9d0a8aeb3fcbe41baa4661e
Kauan Silva <kauanvalentesv@gmail.com>
2024-04-04 21:57:02 -0300

    atualização da classe User

--- Projeto/src/User.py
+++ Projeto/src/User.py
@@ -2,25 +2,33 @@ from run import getconnection  # supondo que collection esteja definida em run.p
 
 class User:
     def __init__(self, name, email, password, gender, phone_number):
-        self.name = name
-        self.email = email
-        self.password = password
-        self.gender = gender
-        self.phone_number = phone_number
-        user_data = {
-            "name": name,
-            "email": email,
-            "gender": gender,
-            "phone_number": phone_number,
-            "password": password,
-        }
-        # Inserindo o usuário no banco de dados
         self.collection = getconnection.get_collection("User")
-        result = self.collection.insert_one(user_data)
-        find = self.collection.find_one({"name": name})
-        print(find["email"])
-
-
+        
+        # Verifica se o usuário já existe no banco de dados com base no número de telefone e e-mail
+        search = {
+            "phone_number": phone_number,
+            "email": email,
+        }
+        isthere_user = self.collection.find_one(search)
+        
+        # Se o usuário não existir, insere os dados do novo usuário no banco de dados
+        if isthere_user is None:
+            user_data = {
+                "name": name,
+                "email": email,
+                "gender": gender,
+                "phone_number": phone_number,
+                "password": password,
+            }
+            self.collection.insert_one(user_data)
+            print("adicionado")
+        else:
+            self.name = isthere_user["name"]
+            self.email = isthere_user["email"]
+            self.password = isthere_user["password"]
+            self.gender = isthere_user["gender"]
+            self.phone_number = isthere_user["phone_number"]
+            print(f"Bem vindo {self.name}")
 
     def get_name(self):
         return self.name
@@ -45,15 +53,15 @@ class User:
         print(email_new)
         self.collection.find_one_and_update({"email":self.email}, {"$set":{"email":email_new}})
     
-    def update_name(self, password_new):
+    def update_password(self, password_new):
         print(password_new)
         self.collection.find_one_and_update({"password":self.password}, {"$set":{"password":password_new}})
     
-    def update_name(self, gender_new):
+    def update_gender(self, gender_new):
         print(gender_new)
         self.collection.find_one_and_update({"gender":self.gender}, {"$set":{"gender":gender_new}})
     
-    def update_name(self, phone_number_new):
+    def update_number(self, phone_number_new):
         print(phone_number_new)
         self.collection.find_one_and_update({"phone_number":self.phone_number}, {"$set":{"phone_number":phone_number_new}})
        
@@ -62,8 +70,6 @@ class User:
         
     
 
-teste = User("Valente", "kauan@gmail.com.br", "Kauan55485", "male", "11 54521365")
-
-teste.update_name("Silva")
+teste = User("Kauan", "aaaaa", "euamookauan", "japones", "11")
 
 
