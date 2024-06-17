151a7fb24f64f83832ff30ab17956f7118c35efb
DESKTOP-V7IDAI2\lucas <lucasgsg15@hotmail.com>
2024-06-16 22:29:34 -0300

    Atualizacao interfaces avaliacao

--- Projeto/src/ZInterfaceAvaliacaoMusica.py
+++ Projeto/src/ZInterfaceAvaliacaoMusica.py
@@ -9,7 +9,7 @@ mainmenu = Interface_main()
 avaliacao = Avaliacao(getconnection)
 search = Interface_search()
 
-class AvaliacaoInterface(Menu):
+class AvaliacaoInterMsc(Menu):
     def __init__(self, idUser, idmusica, idalbum):
         self.user = idUser
         self.musica = idmusica
@@ -23,15 +23,7 @@ class AvaliacaoInterface(Menu):
             "5 - Me mostre os comentários que as pessoas estão fazendo sobre essa música"
         ]
 
-        self.optionsalbum = [
-            "1 - Favoritar esse álbum!!! Gosto muuIIiito dele:",
-            "2 - Remover like, não gosto mais dele (emoji nojo)",
-            "4 - Tenho mt a dizer!!!! Quero comentar essa música",
-            "5 - Me mostre os comentários que as pessoas estão fazendo sobre esse álbum"
-        ]
-
-
-    def inicioMusicOptions(self, idMusica: ObjectId):
+    def iniciotela(self, idMusica: ObjectId):
         print("Abrindo a música!! Aqui estão as informações dela:")
         musica = avaliacao.validar_musica(idMusica)
         if musica["likes"]:
@@ -69,9 +61,9 @@ class AvaliacaoInterface(Menu):
         while True:
             bool_str = input("Deseja avaliar a música? Responda com 1 para sim e 0 para não:\n")
             if bool_str == '1':
-                self.renderMusic()
+                self.render()
                 opcao = input("Digite a opção desejada:")
-                self.nextMusic(opcao)
+                self.next(opcao)
                 break  # Sai do loop enquanto a opção for válida
             elif bool_str == '0':
                 print("Beleza! Voltando ao menu principal...")
@@ -84,7 +76,7 @@ class AvaliacaoInterface(Menu):
                 print("Opção inválida, tente novamente.")
         
 
-    def renderMusic(self):
+    def render(self):
         self.search.display_main_menu()
         margem = '=' * (len(self.title) + 5)
         print(margem)
@@ -109,14 +101,14 @@ class AvaliacaoInterface(Menu):
         elif next_option == '2':
             search.display_main_menu()
             option = int(input("Escolha uma opção: "))
-            self.nextMusic(option)
+            self.next(option)
             
         else:
             print("Opção inválida.")
             self.finalAcao()
 
     
-    def nextMusic(self, option):
+    def next(self, option):
         clear_screen()  # limpa a tela ao iniciar um novo menu
         avaliar = Avaliacao(getconnection)
         
@@ -152,83 +144,6 @@ class AvaliacaoInterface(Menu):
             comentarios = comentariocollection.find_many({"musica": ObjectId(self.musica)})
             for comentario in comentarios:
                 print(comentarios["comentario"])
-
-        else:
-            print("Opção inválida! Tente novamente.")
-            self.finalAcao()
-
-        return self  
-
-    def inicioAlbunsOptions(self, idAlbum: ObjectId):
-        print("Abrindo o álbum!! Aqui estão as informações dele:")
-        album = avaliacao.validar_musica(idAlbum)
-        if album["favoritados"]:
-            print(f"O album {album['album']} foi favoritado {album['favoritados']} vezes!")
-        else:
-            print(f"O album {album["album"]} não foi favoritado ainda... Mude isso!!")
-        print("Outras informações sobre o álbum!! Veja:")
-        print(f"De {album['artista']}, ele possui as seguintes faixas:")
-        faixas = album.get('faixas', [])
-        for idx, faixa in enumerate(faixas, start=1):
-            print(f"{idx} - {faixa}")
-        print("Deseja favoritar esse álbum?")
-
-        while True:
-            bool_str = input("Deseja avaliar esse álbum? Responda com 1 para sim e 0 para não:\n")
-            if bool_str == '1':
-                self.renderAlbum()
-                opcao = input("Digite a opção desejada:")
-                self.nextAlbum(opcao)
-                break  # Sai do loop enquanto a opção for válida
-            elif bool_str == '0':
-                print("Beleza! Voltando ao menu principal...")
-                print("...")
-                print("...")
-                print("...")
-                mainmenu.initial_menu()
-                break  # Sai do loop enquanto a opção for válida
-            else:
-                print("Opção inválida, tente novamente.")
-    
-    def renderAlbum(self):
-        self.search.display_main_menu()
-        margem = '=' * (len(self.title) + 5)
-        print(margem)
-        print(f"|| {self.title} ||")
-        print(margem + "\n")
-
-        for option in self.optionsalbum:
-            print(option)
-    
-    def nextAlbum(self, option):
-        clear_screen()  # limpa a tela ao iniciar um novo menu
-        avaliar = Avaliacao(getconnection)
-        
-        if option == 1:
-
-            like = avaliar.favoritarAlbum(self.album, self.user)
-            print(like)
-            self.finalAcao()
-
-        elif option == 2:
-
-            deslike = avaliar.desfavoritarAlbum(self.album, self.user)
-            print(deslike)
-            self.finalAcao()
-
-        elif option == 3:
-
-            comentario = input("Sou todo ouvidos! Me conta o que você tem a dizer:")
-            comt = avaliar.comentar(self.idmusica, self.user, comentario)
-            print(comt)
-            self.finalAcao()
-        
-        elif option == 4:
-            comentariocollection = self.__db_connection.get_collection("Comentarios")
-            comentarios = comentariocollection.find_many({"albun": ObjectId(self.album)})
-            for comentario in comentarios:
-                print(comentarios["comentario"])
-            self.finalAcao()
 
         else:
             print("Opção inválida! Tente novamente.")
